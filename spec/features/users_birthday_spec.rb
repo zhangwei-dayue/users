d45b29d7786a8b30e14087f35ea2099a553e3ffe
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-09-28 21:07:47 +0800

    添加了little-endian, middle-endian设置下的自动化测试

--- spec/features/users_birthday_spec.rb
+++ spec/features/users_birthday_spec.rb
@@ -5,7 +5,7 @@ feature "测试不同格式生日日期", :type => :feature do
   context "日期格式设置为big-endian" do
     before do
       # 把日期格式设置为big-endian
-      $APP["birthday_format"] == "big-endian"
+      $APP["birthday_format"] = "big-endian"
     end
 
     scenario "注册用户生日格式为:2016-01-11, 预期结果为2016-01-11" do
@@ -56,6 +56,230 @@ feature "测试不同格式生日日期", :type => :feature do
       within("#new_user") do  # 填表单
         fill_in "邮箱", with: "test1@example.com"
         fill_in "生日", with: "89-03 13"
+        fill_in "密码", with: "123456"
+        fill_in "再次确认", with: "123456"
+      end
+
+      click_button "Sign up"
+      # 检查注册成功后的页面文字。看是否有刚更新的用户的生日信息
+
+      expect(page).to have_content("1989-03-13")
+
+      # 检查数据库里面最后一笔真的有刚刚填的资料
+
+      user = User.last
+      expect(user.birthday).to eq("1989-03-13")
+    end
+
+    scenario "注册用户生日格式为:二零一二年十月十二日, 预期结果为2012-10-12" do
+      visit "/users/sign_up"  # 浏览注册页面
+      expect(page).to have_content("Sign up")
+      within("#new_user") do  # 填表单
+        fill_in "邮箱", with: "test1@example.com"
+        fill_in "生日", with: "二零一二年十月十二日"
+        fill_in "密码", with: "123456"
+        fill_in "再次确认", with: "123456"
+      end
+
+      click_button "Sign up"
+      # 检查注册成功后的页面文字。看是否有刚更新的用户的生日信息
+
+      expect(page).to have_content("2012-10-12")
+
+      # 检查数据库里面最后一笔真的有刚刚填的资料
+
+      user = User.last
+      expect(user.birthday).to eq("2012-10-12")
+    end
+
+    scenario "注册用户生日格式为:2011年4月5日, 预期结果为2011-04-05" do
+      visit "/users/sign_up"  # 浏览注册页面
+      expect(page).to have_content("Sign up")
+      within("#new_user") do  # 填表单
+        fill_in "邮箱", with: "test1@example.com"
+        fill_in "生日", with: "2011年4月5日"
+        fill_in "密码", with: "123456"
+        fill_in "再次确认", with: "123456"
+      end
+
+      click_button "Sign up"
+      # 检查注册成功后的页面文字。看是否有刚更新的用户的生日信息
+
+      expect(page).to have_content("2011-04-05")
+
+      # 检查数据库里面最后一笔真的有刚刚填的资料
+
+      user = User.last
+      expect(user.birthday).to eq("2011-04-05")
+    end
+  end
+
+  context "日期格式设置为big-endian" do
+    before do
+      # 把日期格式设置为big-endian
+      $APP["birthday_format"] = "little-endian"
+    end
+
+    scenario "注册用户生日格式为:11-01-2016, 预期结果为2016-01-11" do
+      visit "/users/sign_up"  # 浏览注册页面
+      expect(page).to have_content("Sign up")
+      within("#new_user") do  # 填表单
+        fill_in "邮箱", with: "test1@example.com"
+        fill_in "生日", with: "11-01-2016"
+        fill_in "密码", with: "123456"
+        fill_in "再次确认", with: "123456"
+      end
+
+      click_button "Sign up"
+      # 检查注册成功后的页面文字。看是否有刚更新的用户的生日信息
+
+      expect(page).to have_content("2016-01-11")
+
+      # 检查数据库里面最后一笔真的有刚刚填的资料
+
+      user = User.last
+      expect(user.birthday).to eq("2016-01-11")
+    end
+
+    scenario "注册用户生日格式为:12.2/17, 预期结果为2017-02-12" do
+      visit "/users/sign_up"  # 浏览注册页面
+      expect(page).to have_content("Sign up")
+      within("#new_user") do  # 填表单
+        fill_in "邮箱", with: "test1@example.com"
+        fill_in "生日", with: "12.2/17"
+        fill_in "密码", with: "123456"
+        fill_in "再次确认", with: "123456"
+      end
+
+      click_button "Sign up"
+      # 检查注册成功后的页面文字。看是否有刚更新的用户的生日信息
+
+      expect(page).to have_content("2017-02-12")
+
+      # 检查数据库里面最后一笔真的有刚刚填的资料
+
+      user = User.last
+      expect(user.birthday).to eq("2017-02-12")
+    end
+
+    scenario "注册用户生日格式为:13-03 89, 预期结果为1989-03-13" do
+      visit "/users/sign_up"  # 浏览注册页面
+      expect(page).to have_content("Sign up")
+      within("#new_user") do  # 填表单
+        fill_in "邮箱", with: "test1@example.com"
+        fill_in "生日", with: "13-03 89"
+        fill_in "密码", with: "123456"
+        fill_in "再次确认", with: "123456"
+      end
+
+      click_button "Sign up"
+      # 检查注册成功后的页面文字。看是否有刚更新的用户的生日信息
+
+      expect(page).to have_content("1989-03-13")
+
+      # 检查数据库里面最后一笔真的有刚刚填的资料
+
+      user = User.last
+      expect(user.birthday).to eq("1989-03-13")
+    end
+
+    scenario "注册用户生日格式为:二零一二年十月十二日, 预期结果为2012-10-12" do
+      visit "/users/sign_up"  # 浏览注册页面
+      expect(page).to have_content("Sign up")
+      within("#new_user") do  # 填表单
+        fill_in "邮箱", with: "test1@example.com"
+        fill_in "生日", with: "二零一二年十月十二日"
+        fill_in "密码", with: "123456"
+        fill_in "再次确认", with: "123456"
+      end
+
+      click_button "Sign up"
+      # 检查注册成功后的页面文字。看是否有刚更新的用户的生日信息
+
+      expect(page).to have_content("2012-10-12")
+
+      # 检查数据库里面最后一笔真的有刚刚填的资料
+
+      user = User.last
+      expect(user.birthday).to eq("2012-10-12")
+    end
+
+    scenario "注册用户生日格式为:2011年4月5日, 预期结果为2011-04-05" do
+      visit "/users/sign_up"  # 浏览注册页面
+      expect(page).to have_content("Sign up")
+      within("#new_user") do  # 填表单
+        fill_in "邮箱", with: "test1@example.com"
+        fill_in "生日", with: "2011年4月5日"
+        fill_in "密码", with: "123456"
+        fill_in "再次确认", with: "123456"
+      end
+
+      click_button "Sign up"
+      # 检查注册成功后的页面文字。看是否有刚更新的用户的生日信息
+
+      expect(page).to have_content("2011-04-05")
+
+      # 检查数据库里面最后一笔真的有刚刚填的资料
+
+      user = User.last
+      expect(user.birthday).to eq("2011-04-05")
+    end
+  end
+
+  context "日期格式设置为big-endian" do
+    before do
+      # 把日期格式设置为big-endian
+      $APP["birthday_format"] = "middle-endian"
+    end
+
+    scenario "注册用户生日格式为:01-11-2016, 预期结果为2016-01-11" do
+      visit "/users/sign_up"  # 浏览注册页面
+      expect(page).to have_content("Sign up")
+      within("#new_user") do  # 填表单
+        fill_in "邮箱", with: "test1@example.com"
+        fill_in "生日", with: "01-11-2016"
+        fill_in "密码", with: "123456"
+        fill_in "再次确认", with: "123456"
+      end
+
+      click_button "Sign up"
+      # 检查注册成功后的页面文字。看是否有刚更新的用户的生日信息
+
+      expect(page).to have_content("2016-01-11")
+
+      # 检查数据库里面最后一笔真的有刚刚填的资料
+
+      user = User.last
+      expect(user.birthday).to eq("2016-01-11")
+    end
+
+    scenario "注册用户生日格式为:2.12/17, 预期结果为2017-02-12" do
+      visit "/users/sign_up"  # 浏览注册页面
+      expect(page).to have_content("Sign up")
+      within("#new_user") do  # 填表单
+        fill_in "邮箱", with: "test1@example.com"
+        fill_in "生日", with: "2.12/17"
+        fill_in "密码", with: "123456"
+        fill_in "再次确认", with: "123456"
+      end
+
+      click_button "Sign up"
+      # 检查注册成功后的页面文字。看是否有刚更新的用户的生日信息
+
+      expect(page).to have_content("2017-02-12")
+
+      # 检查数据库里面最后一笔真的有刚刚填的资料
+
+      user = User.last
+      expect(user.birthday).to eq("2017-02-12")
+    end
+
+    scenario "注册用户生日格式为:03-13 89, 预期结果为1989-03-13" do
+      visit "/users/sign_up"  # 浏览注册页面
+      expect(page).to have_content("Sign up")
+      within("#new_user") do  # 填表单
+        fill_in "邮箱", with: "test1@example.com"
+        fill_in "生日", with: "03-13 89"
         fill_in "密码", with: "123456"
         fill_in "再次确认", with: "123456"
       end
